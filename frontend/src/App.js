3dce21e89541df8be7ee9447fce4119181942016
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-13 21:57:54 +0300

    added basic react project for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import BookingPage from './pages/Bookings';
+import EventPage from './pages/Events';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Redirect from="/" to="/auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventPage} />
+          <Route path="/bookings" component={BookingPage} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
