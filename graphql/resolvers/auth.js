eb39880b6e2fb99c7f3046e215b6b73e4999c609
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-13 14:43:34 +0300

    added bcrypt & loginFunction login part I

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs')
 const User = require('../../models/user');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
 
@@ -22,4 +23,22 @@ module.exports = {
       throw err
     }
   },
+  login: async ({ email, password }) => {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error('User does not exists.');
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      throw new Error('Invalid credentials');
+    }
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      'mysupersecretkeyasfollow',
+      {
+        expiresIn: '1h'
+      }
+    );
+    return { userId: user.id, token: token, tokenExpiration: 1 };
+  }
 };
