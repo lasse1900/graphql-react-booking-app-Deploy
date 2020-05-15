448a5a6d2d7a6a2eb97f2e42e39f7ae7c662c52f
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-15 11:46:00 +0300

    adding logout and modal part II

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import './Events.css'
+import Modal from '../components/Modal/Modal';
+
+import './Events.css';
 
 class EventsPage extends Component {
   render() {
     return (
-      <div className="events-control">
-        <p>Share your own events!</p>
-        <button className="btn">Create Event</button>
-      </div>
+      <React.Fragment>
+        <Modal title="Add Event" canCancel canCofitm>Modal Content</Modal>
+        <div className="events-control">
+          <p>Share your own events!</p>
+          <button className="btn">Create Event</button>
+        </div>
+      </React.Fragment>
     );
   }
 }
