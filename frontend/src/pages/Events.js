af8b557aee4d80d67091802ba5b9b39149458b7c
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-15 16:33:57 +0300

    adding events part I

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Modal from '../components/Modal/Modal';
 import Backdrop from '../components/Backdrop/Backdrop';
+import AuthContext from '../context/auth-context';
 import './Events.css';
 
 class EventsPage extends Component {
@@ -9,13 +10,76 @@ class EventsPage extends Component {
     createing: false
   };
 
+  static contextType = AuthContext;
+
+  constructor(props) {
+    super(props);
+    this.titleElRef = React.createRef();
+    this.priceElRef = React.createRef();
+    this.dateElRef = React.createRef();
+    this.descriptionElRef = React.createRef();
+  }
+
   startCreateEventHandler = () => {
     this.setState({ creating: true });
   }
 
   modalConfirmHandler = () => {
     this.setState({ creating: false });
-  }
+    const title = this.titleElRef.current.value;
+    const price = +this.priceElRef.current.value;
+    const date = this.dateElRef.current.value;
+    const description = this.descriptionElRef.current.value;
+
+    if (title.trim().length === 0 || price <= 0 || date.trim().length === 0 || description.trim().length === 0) {
+      return;
+    }
+
+    const event = { title, price, date, description };
+    console.log(event)
+
+    const requestBody = {
+      query: `
+          mutation {
+            createEvent(eventInput: {title: "${title}", description: "${description}", price: ${price}, date: "${date}"}){
+              _id
+              title
+              description
+              price
+              date
+              creator {
+                _id
+                email
+              }
+            }
+          }
+        `
+    };
+
+    const token = this.context.token;
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
+
 
   modalCancelHandler = () => {
     this.setState({ creating: false });
@@ -33,7 +97,24 @@ class EventsPage extends Component {
             onCancel={this.modalCancelHandler}
             onConfirm={this.modalConfirmHandler}
           >
-            <p>Modal Content</p>
+            <form>
+              <div className="form-control">
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" ref={this.titleElRef}></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="price">Price</label>
+                <input type="number" id="price" ref={this.priceElRef}></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="date">Date</label>
+                <input type="date" id="date" ref={this.dateElRef}></input>
+              </div>
+              <div className="form-control">
+                <label htmlFor="description">Description</label>
+                <textarea type="description" rows="4" ref={this.descriptionElRef}></textarea>
+              </div>
+            </form>
           </Modal>}
         <div className="events-control">
           <p>Share your own events!</p>
