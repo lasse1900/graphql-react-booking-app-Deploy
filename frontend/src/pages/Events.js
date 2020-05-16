08592e11af3049f779d38b0d910ea9c9e4cecab6
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-16 13:05:02 +0300

    adding Events (spinner, price, date) part IV

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -4,12 +4,14 @@ import Modal from '../components/Modal/Modal';
 import Backdrop from '../components/Backdrop/Backdrop';
 import AuthContext from '../context/auth-context';
 import EventList from '../components/Events/EventList/EventList';
+import Spinner from '../components/Spinner/Spinner';
 import './Events.css';
 
 class EventsPage extends Component {
   state = {
     createing: false,
-    events: []
+    events: [],
+    isLoading: false
   };
 
   static contextType = AuthContext;
@@ -102,6 +104,7 @@ class EventsPage extends Component {
   }
 
   fetchEvents() {
+    this.setState({ isLoading: true })
     const requestBody = {
       query: `
           query {
@@ -136,10 +139,11 @@ class EventsPage extends Component {
       .then(resData => {
         // console.log(resData);
         const events = resData.data.events;
-        this.setState({ events: events })
+        this.setState({ events: events, isLoading: false })
       })
       .catch(err => {
         console.log(err);
+        this.setState({ isLoading: false })
       })
   };
 
@@ -179,10 +183,13 @@ class EventsPage extends Component {
           <p>Share your own events!</p>
           <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
         </div>}
-        <EventList
-          events={this.state.events}
-          authUserId={this.context.userId}
-        />
+        {this.state.isLoading ? (<Spinner />
+        ) : (
+            <EventList
+              events={this.state.events}
+              authUserId={this.context.userId}
+            />)}
+
       </React.Fragment>
     );
   }
