c5b1378f36d2f01fe0e0c1b25f9e967f82bc89aa
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-17 20:48:57 +0300

    added Chart and fixed a bug in merge.js

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -9,12 +9,18 @@ const eventLoader = new DataLoader((eventIds) => {
 });
 
 const userLoader = new DataLoader(userIds => {
-  return User.find({_id: {$in: userIds}});
+  return User.find({ _id: { $in: userIds } });
 });
 
 const events = async eventIds => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
+    events.sort((a, b) => {
+      return (
+        eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString())
+      );
+    });
+    console.log(events, eventIds)
     return events.map(event => {
       return transformEvent(event);
     });
