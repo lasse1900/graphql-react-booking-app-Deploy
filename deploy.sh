3bf70e786baedf8587e1fc758a509056ffee2c3a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-22 15:16:49 +0300

    some changes part III

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+cd backend
+rm -rf ./build
+cd ../frontend
+rm -rf ./build
+npm run build
+cp -r ./build ../backend/
+cd ..
+git add *
+git commit -m 'new automated build'
+git push -u
+git subtree push --prefix=bckend heroku master
