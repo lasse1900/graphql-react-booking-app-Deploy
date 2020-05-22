6aa94687658be3c2714b6138de3e2ad5fea48071
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-22 13:20:40 +0300

    more deploy changes

---
+++ backend/build/precache-manifest.3453575e27999516c1236071b3dc8f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b059e33a95a01a917926c08ea79c88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb562a24af8b23626168",
+    "url": "/static/css/main.09f7bf4f.chunk.css"
+  },
+  {
+    "revision": "10b81efbeaa64cb863b5",
+    "url": "/static/js/2.7c149bd5.chunk.js"
+  },
+  {
+    "revision": "0fadedb825b50fe9986d1f99d2be7451",
+    "url": "/static/js/2.7c149bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb562a24af8b23626168",
+    "url": "/static/js/main.6443af32.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
